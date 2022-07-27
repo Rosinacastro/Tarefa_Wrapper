6c53c4d1b22ea7aad1a2b509913b78fd9f84e31d
Rosinacastro <rosicastro1@live.com>
2022-07-27 13:34:29 -0300

    first commit

---
+++ src/Primitivo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * @author rosina.castro
+ */
+public class Primitivo {
+
+    public static void main(String args[]) {
+
+        long id = 123654;
+        System.out.println(id);
+        Scanner s = new Scanner(System.in);
+        Long id2 = id;
+        System.out.println(id2);
+
+
+    }
+
+}
